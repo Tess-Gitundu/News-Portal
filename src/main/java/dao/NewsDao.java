d1c2e257e81f23e526f6a4d8c99678fec6d4c410
Tess <njerigitundu003@gmail.com>
2022-05-16 05:45:32 +0300

    Create methods to be implemented in other classes in NewsDao interface

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+
+    //read
+    List<News> getAll();
+    List<News> getAllNewsByEmployee(int employeeId);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
