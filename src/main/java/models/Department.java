b283cab71f3c8e906d144ebc172374ad4098c789
Tess <njerigitundu003@gmail.com>
2022-05-16 04:59:33 +0300

    Create POJO for Department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,61 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private String departmentName;
+    private String description;
+    private String numberOfEmployees;
+    private int id;
+
+    public Department(String departmentName, String description, String numberOfEmployees) {
+        this.departmentName = departmentName;
+        this.description = description;
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(String numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && Objects.equals(departmentName, that.departmentName) && Objects.equals(description, that.description) && Objects.equals(numberOfEmployees, that.numberOfEmployees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName, description, numberOfEmployees, id);
+    }
 }
