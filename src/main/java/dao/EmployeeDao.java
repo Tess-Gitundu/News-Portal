0f8a5284d3706157b74f90a1cc64d636f4bef859
Tess <njerigitundu003@gmail.com>
2022-05-16 05:38:12 +0300

    Create methods to be implemented in other classes in EmployeeDao interface

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Employee;
+
+import java.util.List;
+
 public interface EmployeeDao {
+    //create
+    void add (Employee employee);
+    // void addEmployeeToDepartment(Employee employee, Department department)
+
+    //read
+    List<Employee> getAll();
+    Employee findById(int id);
+    // List<Employee> getAllDepartmentsForAnEmployee(int employeeId);
+
+    //update
+    void update(int id, String employee, String role, String position, String email, int departmentId);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
