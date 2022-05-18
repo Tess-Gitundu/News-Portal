2db73c55169bcd81debf32256b6e4197fa7ff632
Tess <njerigitundu003@gmail.com>
2022-05-18 13:20:12 +0300

    Create tests for the getters in news class

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,30 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewsTest {
+    public  News setUpNews(){
+        News testNews = new News("School", "Closing school", 1);
+        return testNews;
+    }
 
+    @Test
+    void getTitle() {
+        News testNews = setUpNews();
+        assertEquals("School", testNews.getTitle());
+    }
+
+    @Test
+    void getContent() {
+        News testNews = setUpNews();
+        assertEquals("Closing school", testNews.getContent());
+    }
+
+    @Test
+    void getEmployeeId() {
+        News testNews = setUpNews();
+        assertEquals(1, testNews.getEmployeeId());
+    }
 }
