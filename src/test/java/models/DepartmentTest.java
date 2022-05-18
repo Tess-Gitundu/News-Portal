e8947746662f3e06d81a4e67a809335eda848e65
Tess <njerigitundu003@gmail.com>
2022-05-18 13:04:31 +0300

    Create tests for the getters

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,33 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Watchable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentTest {
 
+    public Department setUpDepartment(){
+        Department testDepartment = new Department("Maths", "Science", "2");
+        return testDepartment;
+    }
+
+    @Test
+    void getDepartmentName() {
+        Department testDepartment = setUpDepartment();
+        assertEquals("Maths", testDepartment.getDepartmentName());
+    }
+
+    @Test
+    void getDescription() {
+        Department testDepartment = setUpDepartment();
+        assertEquals("Science", testDepartment.getDescription());
+    }
+
+    @Test
+    void getNumberOfEmployees() {
+        Department testDepartment = setUpDepartment();
+        assertEquals("2", testDepartment.getNumberOfEmployees());
+    }
 }
